62faf46ed7155d980c3785293a257edcd5f08d94
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2025-03-20 15:19:14 -0300

    adiciona poem-langchain

---
+++ poem-langchain/README.md
@@ -0,0 +1,34 @@
+# poem-langchain
+
+## Requisitos
+
+- python 3 e pip 3 instalados
+- uma API Key da Open AI: https://platform.openai.com/settings/organization/api-keys
+- um arquivo `.env` com a propriedade `OPENAI_API_KEY`com sua API Key
+
+## Como executar
+
+Crie um Virtual Environment para o projeto:
+
+```
+python3 -m venv env-poem-langchain
+```
+
+
+Ative o Virtual Environment criado:
+
+```
+source env-poem-langchain/bin/activate
+```
+
+Instale as bibliotecas utilizadas:
+
+```
+pip3 install -r requirements.txt
+```
+
+Execute o código:
+
+```
+python3 main.py
+```
---
+++ poem-langchain/main.py
@@ -0,0 +1,28 @@
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import SystemMessage
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+model = init_chat_model("gpt-4o-mini", model_provider="openai", api_key=os.getenv("OPENAI_API_KEY"))
+
+template = ChatPromptTemplate([
+    SystemMessage("""Você é um poeta escreve versos livres em português brasileiro.
+                    Você irá receber diversos temas do usuário e deverá escrever um poema em que cada verso aborda um dos temas.
+                    Você tem um tom engraçado e sarcástico."""),
+    ("human", "{user_input}")
+])
+
+prompt = template.invoke({
+  "user_input": """Gere um poema sobre os seguintes temas:
+            - Goiás
+            - Jantinha
+            - Cerrado
+            - Família"""
+})
+
+response = model.invoke(prompt)
+print(response.content)
